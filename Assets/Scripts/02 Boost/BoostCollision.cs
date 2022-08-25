581840c8aa2cecee9028452492d6754599ba78d8
Andrew <ajborkowski1031@gmail.com>
2022-08-25 02:04:44 -0400

    Lesson 47 - Using Invoke()

--- Assets/Scripts/02 Boost/BoostCollision.cs
+++ Assets/Scripts/02 Boost/BoostCollision.cs
@@ -3,31 +3,43 @@ using UnityEngine.SceneManagement;
 
 public class BoostCollision : MonoBehaviour
 {
+    [SerializeField] float timeoutDelay = 1f;
+
     private void OnCollisionEnter(Collision other) 
     {
         switch(other.gameObject.tag)
         {
             case "Friendly":
                 //Debug.Log("You hit a friendly.");
-
                 break;
             case "Finish":
                 //Debug.Log("You made it to finish.");
                 LoadNextLevel();
                 break;
             default:
-                Debug.Log("Huh? What? You crashed.");
-                ReloadLevel();
+                //Debug.Log("Huh? What? You crashed.");
+                StartCrashSequence();
                 break;
         }
     }
 
+    void StartCrashSequence()
+    {
+        GetComponent<RocketMovement>().enabled = false;
+        Invoke("ReloadLevel", timeoutDelay);
+    }
+
+    void HitGoal()
+    {
+        GetComponent<RocketMovement>().enabled = false; // optional
+        Invoke("LoadNextLevel", timeoutDelay);
+    }
+
     void ReloadLevel()
     {
-        //SceneManager.LoadScene(0);
-        // !!! This is how you'd reload the current level
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+        GetComponent<RocketMovement>().enabled = true;
     }
 
     void LoadNextLevel()
